733b297abfb2997978ea3d9c280ccda7a81b893f
Roman Cuscenco <kusrs@tavrida.com>
2021-12-16 23:33:04 +0300

    extracting viewmodels

---
+++ SuperPanel.App/ViewModels/GDPRResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SuperPanel.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperPanel.App.ViewModels
+{
+    public class GDPRResultViewModel
+    {
+        public Dictionary<string, User> UpdatedUsers { get; set; } = new Dictionary<string, User>();
+        public List<string> NotFoundUserEmails { get; set; } = new List<string>();
+    }
+}
